765c9c07f7af3dbacfa60206981cf8b6ebc94635
zdeep10 <gabriel.wti02@gmail.com>
2022-10-19 11:49:17 -0300

    Creating partials

---
+++ partials/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mobiltec</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+    <!-- Custom Style -->
+    <link rel="stylesheet" href="assets/css/compressed/global.min.css">
+</head>
+
+<header class="container-fluid" style="background-color: orange ;">
+    <div class="row text-white">
+        <figure class="col-6">
+            <img src="assets/img/logo-mobiltec.png" alt="">
+        </figure>
+        <nav class="col-6">
+            <ul>
+                <li>Home</li>
+                <li>contato</li>
+                <li></li>
+                <li></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+
+<body>
